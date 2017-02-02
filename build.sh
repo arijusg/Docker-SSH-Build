9f451ee545bd7be20ec8e021d4923b5d848feae6
Arijus Gilbrantas <arijus.gilbrantas@aegon.nl>
2017-02-02 13:55:07 +0100

    moved Habitus to lib folder

--- build.sh
+++ build.sh
@@ -1,14 +1,18 @@
+#Get script's' directory path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 #Download Habitus
-wget -nc -O habitus https://github.com/cloud66/habitus/releases/download/v0.4.7/habitus_linux_amd64
-sudo chmod +x habitus
+wget -nc -O ${DIR}/lib/habitus https://github.com/cloud66/habitus/releases/download/v0.4.7/habitus_linux_amd64
+sudo chmod +x ${DIR}/lib/habitus
 
 #Get local IP
 MYIP="$(ip route get 8.8.8.8 | awk '{print $NF; exit}')"
 
 #Build
- ./habitus --use-tls=false \
+ ${DIR}/lib/habitus --use-tls=false \
 --force-rm \
 --force-rmi \
+--f ${DIR}/build.yml \
 --host=unix:///var/run/docker.sock \
 --binding=${MYIP} \
 --build host=${MYIP}
